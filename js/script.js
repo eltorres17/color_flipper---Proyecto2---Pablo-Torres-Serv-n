57b2033413f30ad2a1fc96f143a5a374583df3c9
eltorres17 <pablotorreservan@gmail.com>
2025-02-14 19:27:24 +0100

    explicación js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ document.addEventListener("DOMContentLoaded",function(){
     const letra = document.getElementById("color");
     const body = document.getElementById("body");
     
-
+/*Ahora vamos a crear una tupla con los colores que queremos selecionar aleatoriamente */
     const color = ["Red","Green","Blue","Yellow","Pink","Purple","Orange","Brown","Black","White","burlywood","blanchedalmond","paleturquoise","salmon","tan","goldenrod","lightskyblue","sandybrown","grey","chartreuse"];
 
     
     let i = 1;
 
 
-
+/*Vamos a añadir un evento al boton, haciendo click, para ello declaramos una variable que es random, la cual contiene la funcion de aleatorio */
+/*Ponemos solo random la letra.style.color, debido a que esta ya va a saber antes cual es el color que toca aleatoriamente, los demas contienen el color que se le selecciona anteriormente */
     boton.addEventListener("click", function(){
         
         let random = Math.floor(Math.random()*color.length);
--- js/script_hex.js
+++ js/script_hex.js
@@ -3,15 +3,16 @@ document.addEventListener("DOMContentLoaded",function(){
     const letra = document.getElementById("color");
     const body = document.getElementById("body");
 
+        /* Creamos una tupla con los colores que van a ser aleatorios */
     const color = ["#ff5733","#33ffdd","#efc662","#faaf33","#9033fa","#fb096c","#dde359","#a38b46","#46a381","#7b72a7","#338CFF","#FF4500","#32CD42","#1E90FF","#8A2BE2","#8A2BE2","#20B2AA","#FFD700","#DC143C","#00FA9A"];
 
     let i = 1;
-
+        /* Usamos los valores determinados para que este color sea otro al seleccionar el html hexadecimal */
     body.style.background = "#33ffdd";
     letra.innerHTML = "#33ffdd";
     boton.style.background = "#33ffdd";
 
-
+        /* Añadimos una funcion al boton para que la seleccion del color sea aleatoria, esta función lo que hace es: buscar de manera alatoria el siguiente color en la lista de colores por su longitud */
     
     boton.addEventListener("click", function(){
         
